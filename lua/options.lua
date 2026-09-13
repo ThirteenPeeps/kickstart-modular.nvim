a997448b5cf2043515887810a8e45f9fac1d9241
Juniper Reese <38056103+ThirteenPeeps@users.noreply.github.com>
2026-09-13 03:52:21 -0700

    Remove noisy comments from options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -1,35 +1,8 @@
 -- Enable faster startup by caching compiled Lua modules
 vim.loader.enable()
 
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
--- WARN: obviated by mini.basics (basic)
--- vim.g.mapleader = ' '
--- vim.g.maplocalleader = ' '
-
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
-
--- [[ Setting options ]]
---  See `:help vim.o`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
-
--- Make line numbers default
--- WARN: obviated by mini.basics (basic)
--- vim.o.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
--- WARN: obviated by mini.basics (basic)
--- vim.o.mouse = 'a'
-
--- Don't show the mode, since it's already in the status line
--- WARN: obviated by mini.basics (basic)
--- vim.o.showmode = false
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
@@ -37,52 +10,14 @@ vim.g.have_nerd_font = true
 --  See `:help 'clipboard'`
 vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
 
--- Enable break indent
--- WARN: obviated by mini.basics (basic)
--- vim.o.breakindent = true
-
--- Enable undo/redo changes even after closing and reopening a file
--- WARN: obviated by mini.basics (basic)
--- vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
--- WARN: obviated by mini.basics (basic)
--- vim.o.ignorecase = true
--- vim.o.smartcase = true
-
--- Keep signcolumn on by default
--- WARN: obviated by mini.basics (basic)
--- vim.o.signcolumn = 'yes'
-
 -- Decrease update time
 vim.o.updatetime = 250
 
 -- Decrease mapped sequence wait time
 vim.o.timeoutlen = 300
 
--- Configure how new splits should be opened
--- WARN: obviated by mini.basics (basic)
--- vim.o.splitright = true
--- vim.o.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
---
---  Notice listchars is set using `vim.opt` instead of `vim.o`.
---  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
---   See `:help lua-options`
---   and `:help lua-guide-options`
--- WARN: obviated by mini.basics (extra_ui)
--- vim.o.list = true
--- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
-
--- Show which line your cursor is on
--- WARN: obviated by mini.basics (basics)
--- vim.o.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 0
