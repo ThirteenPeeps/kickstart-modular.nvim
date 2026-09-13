b5ae8a8bf6943fd397afec82b39610844e1f0a75
Juniper Reese <38056103+ThirteenPeeps@users.noreply.github.com>
2026-09-13 02:10:23 -0700

    Modify colorscheme slightly

--- lua/custom/plugins/dracula.lua
+++ lua/custom/plugins/dracula.lua
@@ -4,6 +4,18 @@ require('dracula').setup {
   transparent_bg = true,
   show_end_of_buffer = true,
   italic_comment = true,
+
+  overrides = function (colors)
+    return {
+    MiniStatuslineDevinfo = { fg = colors.purple, bg = colors.menu },
+    MiniStatuslineFilename = { bold = true, fg = colors.white, bg = colors.menu },
+    MiniStatuslineFileinfo = { fg = colors.purple, bg = colors.menu },
+    MiniStatuslineInactive = { fg = colors.white, bg = colors.menu },
+    CursorLineNr = { bold = true, fg = colors.white, bg = colors.bg },
+    CursorLine = { bg = colors.menu },
+    ColorColumn = { bg = colors.menu },
+  }
+  end,
 }
 
 vim.cmd.colorscheme 'dracula'
