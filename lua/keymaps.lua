de783b1420f39c84bb70bcc6ac7bf494dd7d5d93
Juniper Reese <38056103+ThirteenPeeps@users.noreply.github.com>
2026-09-13 03:54:27 -0700

    Comment out obviated code

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -86,10 +86,10 @@ vim.keymap.set('i', '<S-Right>', '<C-o>W', { desc = 'Move forward one WORD' })
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
 --  WARN: obviated by mini.basics (autocommands.basic)
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function() vim.hl.on_yank() end,
-})
+-- vim.api.nvim_create_autocmd('TextYankPost', {
+--   desc = 'Highlight when yanking (copying) text',
+--   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+--   callback = function() vim.hl.on_yank() end,
+-- })
 
 -- vim: ts=2 sts=2 sw=2 et
