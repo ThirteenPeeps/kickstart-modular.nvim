975526f711c27818dbcc0402037d4d8042169a28
Juniper Reese <38056103+ThirteenPeeps@users.noreply.github.com>
2026-09-13 02:12:04 -0700

    Change indent_line config to show exact scope

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -3,4 +3,10 @@
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help ibl`
 vim.pack.add { 'https://github.com/lukas-reineke/indent-blankline.nvim' }
-require('ibl').setup {}
+
+require("ibl").setup {
+  scope = {
+    show_exact_scope = true,
+  }
+}
+
